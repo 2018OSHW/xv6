755eec774f123eb906a81cf50e3634f6c09d5d97
A-Dying-Pig <32242762+A-Dying-Pig@users.noreply.github.com>
2018-06-28 19:06:46 +0800

    character move

--- xv6/kbd.h
+++ xv6/kbd.h
@@ -1,6 +1,6 @@
 // PC keyboard interface constants
 
-#include "PVCObject.h"
+#include "APInclude.h"
 
 #define KBSTATP         0x64    // kbd controller status port(I)
 #define KBS_DIB         0x01    // kbd data in buffer
@@ -35,74 +35,101 @@
 
 static uchar shiftcode[256] =
 {
-    [0x1D] CTL,
-    [0x2A] SHIFT,
-    [0x36] SHIFT,
-    [0x38] ALT,
-    [0x9D] CTL,
-    [0xB8] ALT
+  [0x1D] CTL,
+  [0x2A] SHIFT,
+  [0x36] SHIFT,
+  [0x38] ALT,
+  [0x9D] CTL,
+  [0xB8] ALT
 };
 
 static uchar togglecode[256] =
 {
-    [0x3A] CAPSLOCK,
-    [0x45] NUMLOCK,
-    [0x46] SCROLLLOCK
+  [0x3A] CAPSLOCK,
+  [0x45] NUMLOCK,
+  [0x46] SCROLLLOCK
 };
 
 static uchar normalmap[256] =
 {
-    NO,   0x1B, '1',  '2',  '3',  '4',  '5',  '6',  // 0x00
-    '7',  '8',  '9',  '0',  '-',  '=',  '\b', '\t',
-    'q',  'w',  'e',  'r',  't',  'y',  'u',  'i',  // 0x10
-    'o',  'p',  '[',  ']',  '\n', NO,   'a',  's',
-    'd',  'f',  'g',  'h',  'j',  'k',  'l',  ';',  // 0x20
-    '\'', '`',  NO,   '\\', 'z',  'x',  'c',  'v',
-    'b',  'n',  'm',  ',',  '.',  '/',  NO,   '*',  // 0x30
-    NO,   ' ',  NO,   NO,   NO,   NO,   NO,   NO,
-    NO,   NO,   NO,   NO,   NO,   NO,   NO,   '7',  // 0x40
-    '8',  '9',  '-',  '4',  '5',  '6',  '+',  '1',
-    '2',  '3',  '0',  '.',  NO,   NO,   NO,   NO,   // 0x50
-    [0x9C] '\n',      // KP_Enter
-    [0xB5] '/',       // KP_Div
-    [0xC8] KEY_UP,    [0xD0] KEY_DN,
-    [0xC9] KEY_PGUP,  [0xD1] KEY_PGDN,
-    [0xCB] KEY_LF,    [0xCD] KEY_RT,
-    [0x97] KEY_HOME,  [0xCF] KEY_END,
-    [0xD2] KEY_INS,   [0xD3] KEY_DEL
+  NO,   0x1B, '1',  '2',  '3',  '4',  '5',  '6',  // 0x00
+  '7',  '8',  '9',  '0',  '-',  '=',  '\b', '\t',
+  'q',  'w',  'e',  'r',  't',  'y',  'u',  'i',  // 0x10
+  'o',  'p',  '[',  ']',  '\n', NO,   'a',  's',
+  'd',  'f',  'g',  'h',  'j',  'k',  'l',  ';',  // 0x20
+  '\'', '`',  NO,   '\\', 'z',  'x',  'c',  'v',
+  'b',  'n',  'm',  ',',  '.',  '/',  NO,   '*',  // 0x30
+  NO,   ' ',  NO,   NO,   NO,   NO,   NO,   NO,
+  NO,   NO,   NO,   NO,   NO,   NO,   NO,   '7',  // 0x40
+  '8',  '9',  '-',  '4',  '5',  '6',  '+',  '1',
+  '2',  '3',  '0',  '.',  NO,   NO,   NO,   NO,   // 0x50
+  [0x9C] '\n',      // KP_Enter
+  [0xB5] '/',       // KP_Div
+  [0xC8] KEY_UP,    [0xD0] KEY_DN,
+  [0xC9] KEY_PGUP,  [0xD1] KEY_PGDN,
+  [0xCB] KEY_LF,    [0xCD] KEY_RT,
+  [0x97] KEY_HOME,  [0xCF] KEY_END,
+  [0xD2] KEY_INS,   [0xD3] KEY_DEL
 };
 
 static uchar shiftmap[256] =
 {
-    NO,   033,  '!',  '@',  '#',  '$',  '%',  '^',  // 0x00
-    '&',  '*',  '(',  ')',  '_',  '+',  '\b', '\t',
-    'Q',  'W',  'E',  'R',  'T',  'Y',  'U',  'I',  // 0x10
-    'O',  'P',  '{',  '}',  '\n', NO,   'A',  'S',
-    'D',  'F',  'G',  'H',  'J',  'K',  'L',  ':',  // 0x20
-    '"',  '~',  NO,   '|',  'Z',  'X',  'C',  'V',
-    'B',  'N',  'M',  '<',  '>',  '?',  NO,   '*',  // 0x30
-    NO,   ' ',  NO,   NO,   NO,   NO,   NO,   NO,
-    NO,   NO,   NO,   NO,   NO,   NO,   NO,   '7',  // 0x40
-    '8',  '9',  '-',  '4',  '5',  '6',  '+',  '1',
-    '2',  '3',  '0',  '.',  NO,   NO,   NO,   NO,   // 0x50
-    [0x9C] '\n',      // KP_Enter
-    [0xB5] '/',       // KP_Div
-    [0xC8] KEY_UP,    [0xD0] KEY_DN,
-    [0xC9] KEY_PGUP,  [0xD1] KEY_PGDN,
-    [0xCB] KEY_LF,    [0xCD] KEY_RT,
-    [0x97] KEY_HOME,  [0xCF] KEY_END,
-    [0xD2] KEY_INS,   [0xD3] KEY_DEL
+  NO,   033,  '!',  '@',  '#',  '$',  '%',  '^',  // 0x00
+  '&',  '*',  '(',  ')',  '_',  '+',  '\b', '\t',
+  'Q',  'W',  'E',  'R',  'T',  'Y',  'U',  'I',  // 0x10
+  'O',  'P',  '{',  '}',  '\n', NO,   'A',  'S',
+  'D',  'F',  'G',  'H',  'J',  'K',  'L',  ':',  // 0x20
+  '"',  '~',  NO,   '|',  'Z',  'X',  'C',  'V',
+  'B',  'N',  'M',  '<',  '>',  '?',  NO,   '*',  // 0x30
+  NO,   ' ',  NO,   NO,   NO,   NO,   NO,   NO,
+  NO,   NO,   NO,   NO,   NO,   NO,   NO,   '7',  // 0x40
+  '8',  '9',  '-',  '4',  '5',  '6',  '+',  '1',
+  '2',  '3',  '0',  '.',  NO,   NO,   NO,   NO,   // 0x50
+  [0x9C] '\n',      // KP_Enter
+  [0xB5] '/',       // KP_Div
+  [0xC8] KEY_UP,    [0xD0] KEY_DN,
+  [0xC9] KEY_PGUP,  [0xD1] KEY_PGDN,
+  [0xCB] KEY_LF,    [0xCD] KEY_RT,
+  [0x97] KEY_HOME,  [0xCF] KEY_END,
+  [0xD2] KEY_INS,   [0xD3] KEY_DEL
 };
 
 static uchar ctlmap[256] =
 {
-    NO,      NO,      NO,      NO,      NO,      NO,      NO,      NO,
-    NO,      NO,      NO,      NO,      NO,      NO,      NO,      NO,
-    C('Q'),  C('W'),  C('E'),  C('R'),  C('T'),  C('Y'),  C('U'),  C('I'),
-    C('O'),  C('P'),  NO,      NO,      '\r',    NO,      C('A'),  C('S'),
-    C('D'),  C('F'),  C('G'),  C('H'),  C('J'),  C('K'),  C('L'),  NO,
-    NO,      NO,      NO,      C('\\'), C('Z'),  C('X'),  C('C'),  C('V'),
-    C('B'),  C('N'),  C('M'),  NO,      NO,      C('/'),  NO,      NO,
+  NO,      NO,      NO,      NO,      NO,      NO,      NO,      NO,
+  NO,      NO,      NO,      NO,      NO,      NO,      NO,      NO,
+  C('Q'),  C('W'),  C('E'),  C('R'),  C('T'),  C('Y'),  C('U'),  C('I'),
+  C('O'),  C('P'),  NO,      NO,      '\r',    NO,      C('A'),  C('S'),
+  C('D'),  C('F'),  C('G'),  C('H'),  C('J'),  C('K'),  C('L'),  NO,
+  NO,      NO,      NO,      C('\\'), C('Z'),  C('X'),  C('C'),  C('V'),
+  C('B'),  C('N'),  C('M'),  NO,      NO,      C('/'),  NO,      NO,
+  [0x9C] '\r',      // KP_Enter
+  [0xB5] C('/'),    // KP_Div
+  [0xC8] KEY_UP,    [0xD0] KEY_DN,
+  [0xC9] KEY_PGUP,  [0xD1] KEY_PGDN,
+  [0xCB] KEY_LF,    [0xCD] KEY_RT,
+  [0x97] KEY_HOME,  [0xCF] KEY_END,
+  [0xD2] KEY_INS,   [0xD3] KEY_DEL
+};
+
+static uchar normalMap[256] =
+{
+	NO,   VK_ESC, NO,  NO,  NO,  NO,  NO,  NO,  // 0x00
+	NO,  NO,  NO,  NO,  NO,  NO, NO, NO,
+	NO,   'w', NO,  NO,  NO,  NO,  NO,  NO,   // 0x10
+	NO,   NO, NO,  NO,  VK_ENTER, NO,  'a',  's', 
+	'd',   NO, NO,  NO,  NO,  NO,  NO,  NO,   // 0x20
+	NO,   NO, NO,  NO,  NO,  NO,  NO,  NO,
+	NO,   NO, NO,  NO,  NO,  NO,  NO,  NO,   // 0x30
+	NO,   NO, NO,  NO,  NO,  NO,  NO,  NO,
+	NO,   NO, NO,  NO,  NO,  NO,  NO,  NO,  // 0x40
+	NO,   NO, NO,  NO,  NO,  NO,  NO,  NO,
+	NO,   NO, NO,  NO,  NO,  NO,  NO,  NO,    // 0x50
+	NO
+};
+
+static uchar e0Map[256] =
+{
     [0x9C] '\r',      // KP_Enter
     [0xB5] C('/'),    // KP_Div
     [0xC8] KEY_UP,    [0xD0] KEY_DN,
@@ -113,32 +140,3 @@ static uchar ctlmap[256] =
 };
 
 void kbdInterupt();
-
-static uchar normalMap[256] =
-{
-    NO,   VK_ESC, '1',  '2',  '3',  '4',  '5',  '6',  // 0x00
-    '7',  '8',  '9',  '0',  '-',  '=',  VK_BACKSPACE, VK_TAB,
-    'q',  'w',  'e',  'r',  't',  'y',  'u',  'i',  // 0x10
-    'o',  'p',  '[',  ']',  VK_ENTER, VK_LCTRL,   'a',  's',
-    'd',  'f',  'g',  'h',  'j',  'k',  'l',  ';',  // 0x20
-    '\'', '`',  VK_LSHIFT,   '\\', 'z',  'x',  'c',  'v',
-    'b',  'n',  'm',  ',',  '.',  '/',  VK_RSHIFT,   VK_MULTIPLY,  // 0x30
-    VK_LALT, VK_SPACE, VK_CAPSLOCK, VK_F1, VK_F2, VK_F3, VK_F4, VK_F5,
-    VK_F6, VK_F7, VK_F8, VK_F9, VK_F10, VK_NUMLOCK, VK_SCROLLLOCK, VK_NUMPAD7,  // 0x40
-    VK_NUMPAD8, VK_NUMPAD9, VK_MINUS, VK_NUMPAD4, VK_NUMPAD5, VK_NUMPAD6, VK_ADD, VK_NUMPAD1,
-    VK_NUMPAD2,  VK_NUMPAD3,  VK_NUMPAD0,  VK_DOT,  NO,   NO,   NO,   VK_F11,   // 0x50
-    VK_F12
-};
-
-static uchar e0Map[256] =
-{
-    [0x5B] VK_LGUI,   [0x1D] VK_RCTRL,
-    [0x5C] VK_RGUI,   [0x38] VK_RALT,
-    [0x5D] VK_APPS,   [0x2A] VK_PRINTSCEEN, [0x37] VK_PRINTSCEEN,
-    [0x52] VK_INSERT, [0x47] VK_HOME,
-    [0x49] VK_PAGEUP, [0x53] VK_DELETE,
-    [0x4F] VK_END,    [0x51] VK_PAGEDOWN,
-    [0x48] VK_UP,     [0x4B] VK_LEFT,
-    [0x50] VK_DOWN,   [0x4D] VK_RIGHT,
-    [0x35] VK_DIVIDE, [0x1C] VK_RETURN
-};
