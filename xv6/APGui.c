fa40f51f21f66541b6c60c24dacb9fcf06133ac3
A-Dying-Pig <32242762+A-Dying-Pig@users.noreply.github.com>
2018-06-29 16:25:30 +0800

    esc control

--- xv6/APGui.c
+++ xv6/APGui.c
@@ -292,6 +292,7 @@ void APGuiInit(void)
     initlock(&screenLock,"sreenLock");
     APCharacterInit();
     APTimerListInit(&timerList);
+    APWndListInit(&wndList);
 }
 
 //将左上角坐标为(x1,y1),右下角坐标为(x2,y2)的矩形区域从Buf绘制到屏幕上
