b799f463b51a07b49af39652ddccd4e9924d19e5
A-Dying-Pig <leiyr16@163.com>
2018-06-29 16:11:19 +0800

    debug

--- xv6/APWindow.c
+++ xv6/APWindow.c
@@ -120,10 +120,6 @@ void APWndExec(AHwnd hwnd, bool (*wndProc)(AHwnd, AMessage))
 void APGridPaint(AHwnd wnd)
 {
     
-    AFont font;
-    font.color = RGB(0x08,0x08,0x08);
-    APSetFont(&wnd->TitleDc,font);
-    APDrawText(&wnd->TitleDc,wnd->title,20,20);
     
     if (!iconReady)
     {
@@ -217,6 +213,12 @@ void APGridPaint(AHwnd wnd)
     APSetPen(&wnd->TitleDc,pen);
     APSetBrush(&wnd->TitleDc,brush);
     APDrawRect(&wnd->TitleDc,0,0,SCREEN_WIDTH,WND_TITLE_HEIGHT);
+
+
+    AFont font;
+    font.color = RGB(0x08,0x08,0x08);
+    APSetFont(&wnd->TitleDc,font);
+    APDrawText(&wnd->TitleDc,wnd->title,20,20);
     
     
 }
