2f489ed5107462e56f95761db079c7bdf2cd528c
xiaoxd16 <xiaoxd16@163.com>
2018-06-29 18:22:53 +0800

    clean

--- xv6/APDesktop.d
+++
@@ -1,3 +0,0 @@
-APDesktop.o: APDesktop.c /usr/include/stdc-predef.h APDesktop.h fcntl.h \
- types.h user.h stat.h fs.h APGui.h APObject.h APInclude.h spinlock.h \
- APPaint.h APLib.h APWindow.h
--- xv6/APMagicTower.d
+++
@@ -1,3 +0,0 @@
-APMagicTower.o: APMagicTower.c /usr/include/stdc-predef.h APMagicTower.h \
- APInclude.h APObject.h types.h spinlock.h user.h APGui.h APPaint.h \
- APLib.h APWindow.h
