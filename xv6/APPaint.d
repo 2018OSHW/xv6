3c8a50a743edb67cf5fe58e6e96417181a5e7a2c
xiaoxd16 <xiaoxd16@163.com>
2018-06-29 17:35:59 +0800

    clean

--- xv6/APPaint.d
+++
@@ -1,2 +0,0 @@
-APPaint.o: APPaint.c /usr/include/stdc-predef.h APPaint.h APObject.h \
- types.h APInclude.h spinlock.h user.h fcntl.h stat.h
--- xv6/APWindow.d
+++
@@ -1,2 +0,0 @@
-APWindow.o: APWindow.c /usr/include/stdc-predef.h types.h user.h \
- APWindow.h APInclude.h APObject.h spinlock.h APPaint.h
